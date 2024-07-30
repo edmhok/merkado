f73e22bbb064a3f05db348a0c9448462df9a645c
mhok <edzone.ece@gmail.com>
2024-07-30 10:39:51 +0800

    swap_add

--- src/routes/config.ts
+++ src/routes/config.ts
@@ -4,6 +4,22 @@ import Loadable from "../components/Loading/Loadable";
 const Home = Loadable(lazy(() => import("../views/home")));
 const Collection = Loadable(lazy(() => import("../views/collection/index")));
 
+const Swap = Loadable(lazy(() => import("../views/swap-liquidity-v3/Swap")));
+const Liquidity = Loadable(lazy(() => import("../views/swap-liquidity-v3/liquidity/index")));
+const AddLiquidity = Loadable(lazy(() => import("../views/swap-liquidity-v3/liquidity/AddLiquidity")));
+const IncreaseLiquidity = Loadable(lazy(() => import("../views/swap-liquidity-v3/liquidity/IncreaseLiquidity")));
+const DecreaseLiquidity = Loadable(lazy(() => import("../views/swap-liquidity-v3/liquidity/DecreaseLiquidity")));
+const SwapReclaim = Loadable(lazy(() => import("../views/swap-liquidity-v3/Reclaim")));
+const SwapFindMisTransferToken = Loadable(lazy(() => import("../views/swap-liquidity-v3/MisTransferTokens")));
+const SwapRevokeApprove = Loadable(lazy(() => import("../views/swap-liquidity-v3/RevokeApprove")));
+const PCMReclaim = Loadable(lazy(() => import("../views/swap-liquidity-v3/PCMReclaim")));
+
+const LiquidityV2 = Loadable(lazy(() => import("../views/swap-v2/liquidity/index")));
+const AddLiquidityV2 = Loadable(lazy(() => import("../views/swap-v2/liquidity/AddLiquidity")));
+const IncreaseLiquidityV2 = Loadable(lazy(() => import("../views/swap-v2/liquidity/IncreaseLiquidity")));
+const DecreaseLiquidityV2 = Loadable(lazy(() => import("../views/swap-v2/liquidity/DecreaseLiquidity")));
+const Wrap = Loadable(lazy(() => import("../views/swap-v2/wrap/index")));
+
 const NFTView = Loadable(lazy(() => import("../views/nft/View")));
 const WalletNFTView = Loadable(lazy(() => import("../views/nft/WalletNFTView")));
 const NFTMint = Loadable(lazy(() => import("../views/nft/Mint")));
@@ -22,6 +38,22 @@ export const routeConfigs: { [path: string]: (props: any) => JSX.Element } = {
 
   "/wallet/nft/view/:canisterId/:tokenId": WalletNFTView,
   "/wallet/nft/canister/details/:id": NFTCanisterDetails,
+
+  "/swap": Swap,
+  "/swap/liquidity": Liquidity,
+  "/swap/liquidity/add/:currencyIdA?/:currencyIdB?/:feeAmount?": AddLiquidity,
+  "/swap/liquidity/decrease/:positionId/:pool": DecreaseLiquidity,
+  "/swap/liquidity/increase/:positionId/:pool": IncreaseLiquidity,
+  "/swap/reclaim": SwapReclaim,
+  "/swap/find-mis-transferred-token": SwapFindMisTransferToken,
+  "/swap/revoke-approve": SwapRevokeApprove,
+  "/swap/pcm/reclaim": PCMReclaim,
+
+  "/swap/v2/liquidity": LiquidityV2,
+  "/swap/v2/liquidity/add/:currencyIdA?/:currencyIdB?/:feeAmount?": AddLiquidityV2,
+  "/swap/v2/liquidity/decrease/:positionId?": DecreaseLiquidityV2,
+  "/swap/v2/liquidity/increase/:positionId?": IncreaseLiquidityV2,
+  "/swap/v2/wrap": Wrap,
 
   "/marketplace/NFT": NFTMarket,
   "/marketplace/NFT/:canisterId": NFTCollectMarket,
---
+++ src/views/swap-liquidity-v3/liquidity/DecreaseLiquidity/Input.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { Grid, Box, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { NumberTextField, TokenImage } from "components/index";
+import { MAX_SWAP_INPUT_LENGTH, SAFE_DECIMALS_LENGTH } from "constants/index";
+import { Trans } from "@lingui/macro";
+import { Theme } from "@mui/material/styles";
+import { Token } from "@w2e/swap-sdk";
+
+const useStyle = makeStyles((theme: Theme) => {
+  return {
+    inputBox: {
+      backgroundColor: theme.palette.background.level3,
+      borderRadius: "12px",
+      padding: "16px 24px 16px 12px",
+    },
+    tokenButton: {
+      backgroundColor: theme.palette.background.level2,
+      borderRadius: "12px",
+      padding: "9px 28px 9px 12px",
+    },
+    input: {
+      "& input": {
+        textAlign: "right",
+        fontSize: "20px",
+        fontWeight: 700,
+        // color: theme.textPrimary,
+      },
+      "& input::placeholder": {
+        fontSize: "20px",
+        fontWeight: 700,
+      },
+    },
+  };
+});
+
+export interface DecreaseLiquidityInputProps {
+  currency: Token | undefined;
+  value: string | number;
+  onUserInput: (value: string) => void;
+  totalAmount: React.ReactChild;
+}
+
+export default function DecreaseLiquidityInput({
+  currency,
+  value,
+  onUserInput,
+  totalAmount,
+}: DecreaseLiquidityInputProps) {
+  const classes = useStyle();
+
+  const decimals = currency?.decimals ?? SAFE_DECIMALS_LENGTH;
+
+  return (
+    <Box className={classes.inputBox}>
+      <Grid container>
+        <Box className={classes.tokenButton}>
+          <Grid item xs container alignItems="center" gap="0 8px">
+            <TokenImage logo={currency?.logo} tokenId={currency?.wrapped.address} size="24px" />
+            <Typography component="span">{currency?.symbol}</Typography>
+          </Grid>
+        </Box>
+        <Grid item xs>
+          <NumberTextField
+            fullWidth
+            value={value}
+            className={classes.input}
+            placeholder="0.0"
+            variant="standard"
+            numericProps={{
+              thousandSeparator: true,
+              decimalScale: decimals > SAFE_DECIMALS_LENGTH ? SAFE_DECIMALS_LENGTH : decimals,
+              allowNegative: false,
+              maxLength: MAX_SWAP_INPUT_LENGTH,
+            }}
+            InputProps={{
+              disableUnderline: true,
+            }}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              onUserInput(e.target.value);
+            }}
+          />
+        </Grid>
+      </Grid>
+      <Box mt={1}>
+        <Typography fontSize="12px">
+          <Trans>Total Amount:</Trans> {totalAmount} {currency?.symbol}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
